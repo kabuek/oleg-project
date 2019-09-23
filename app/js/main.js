eea0e40670286016146823cc7d9dabd0e0cc30ef
kabuek <32717096+kabuek@users.noreply.github.com>
2019-09-23 23:36:34 +0300

    push

---
+++ app/js/main.js
@@ -0,0 +1,18 @@
+$('.portfolio .slider_wrap').slick({
+    infinite: true,
+    arrows: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000
+});
+
+$('.testimonials .testimonials_slider').slick({
+    infinite: true,
+    arrows: false,
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000
+});
